4a1d408054bece477a74be8eaf9a718172681852
LeDHuy <ldhuy230405@gmail.com>
2025-05-29 14:35:28 +0700

    Delete src directory

--- src/FlashcardApp.java
+++
@@ -1,9 +0,0 @@
-import ui.FlashcardUI;
-
-import javax.swing.*;
-
-public class FlashcardApp {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new FlashcardUI().initUI());
-    }
-}
